0afdbdac851650ff6dea254dc274c712849af22a
willy836 <wilsonkimiri836@gmail.com>
2023-02-01 14:24:24 +0300

    Config and basic setup

---
+++ src/components/Missions.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Missions = () => (
+  <div>
+    <h1>This is Missions component</h1>
+  </div>
+);
+
+export default Missions;
---
+++ src/components/MyProfile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyProfile = () => (
+  <div>
+    <h1>This is My Profile component</h1>
+  </div>
+);
+
+export default MyProfile;
