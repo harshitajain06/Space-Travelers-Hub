c93c08a26efbfb37cfd4a7c8711a48345689addd
willy836 <wilsonkimiri836@gmail.com>
2023-02-01 18:51:55 +0300

    Solve merge conflict

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/configureStore";
@@ -11,16 +10,5 @@ root.render(
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
-);
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
   </React.StrictMode>
 );
